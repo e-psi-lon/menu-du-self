4d69d210fea13fb0f9b381a9e0636fe59eda460b
e-psi-lon <theg41g@gmail.com>
2023-03-27 20:19:05 +0200

    Some changes (I really love this 2 words)

--- app/src/main/java/com/android/selfdessaignes/MainActivity.kt
+++ app/src/main/java/com/android/selfdessaignes/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import java.io.IOException
 import java.util.*
 import kotlin.collections.ArrayList
@@ -21,10 +22,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var backButton: Button
     private lateinit var nextButton: Button
     private lateinit var openMenuButton: Button
+    private lateinit var switchToDinner: SwitchCompat
+    private lateinit var appVersion: TextView
+    private lateinit var mainLayout: LinearLayout
     private val appVersionName: String = BuildConfig.VERSION_NAME
     private val appVersionCode: Int = BuildConfig.VERSION_CODE
     private val dayInWeek: List<String> = listOf("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche")
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,13 +40,24 @@ class MainActivity : AppCompatActivity() {
         backButton = findViewById(R.id.back)
         nextButton = findViewById(R.id.next)
         openMenuButton = findViewById(R.id.mainMenu)
+        switchToDinner = findViewById(R.id.switchToDinner)
+        appVersion = findViewById(R.id.currentVersion)
+
+        if (appVersionCode > 10) {
+            var updateAvailable = true
+            while (updateAvailable) {
+                Toast.makeText(this, "Une mise à jour est disponible", Toast.LENGTH_LONG).show()
+                updateAvailable = false
+            }
+        }
+        mainLayout = findViewById(R.id.mainPage)
         currentDay = getDayOfWeek()
-        getMenuFromWebsite(currentDay)
+        executeForXDay(currentDay)
         backButton.setOnClickListener {
             when (currentDay) {
                 "Lundi" -> {}
                 else -> {
-                    getMenuFromWebsite(dayInWeek[dayInWeek.indexOf(currentDay) - 1])
+                    executeForXDay(dayInWeek[dayInWeek.indexOf(currentDay) - 1])
                     currentDay = dayInWeek[dayInWeek.indexOf(currentDay) - 1]
                 }
             }
@@ -50,13 +66,20 @@ class MainActivity : AppCompatActivity() {
             when (currentDay) {
                 "Dimanche" -> {}
                 else -> {
-                    getMenuFromWebsite(dayInWeek[dayInWeek.indexOf(currentDay) + 1])
+                    executeForXDay(dayInWeek[dayInWeek.indexOf(currentDay) + 1])
                     currentDay = dayInWeek[dayInWeek.indexOf(currentDay) + 1]
                 }
             }
 
         }
-        openMenuButton
+        openMenuButton.setOnClickListener {
+            for (Views in listOf(menuTextView,menuCurrentState,menuListView,dayView,backButton,nextButton,openMenuButton)){
+                Views.visibility = View.GONE
+            }
+            switchToDinner.visibility = View.VISIBLE
+            appVersion.visibility = View.VISIBLE
+            appVersion.text = getString(R.string.version)+" "+appVersionName
+        }
 
     }
     private fun getDayOfWeek(): String {
@@ -71,129 +94,67 @@ class MainActivity : AppCompatActivity() {
             else -> ""
         }
     }
-    @SuppressLint("SetTextI18n")
-    private fun getMenuFromWebsite(day: String) = CoroutineScope(Dispatchers.IO).launch {
+
+    private fun getMenu(table:Int,cell:Int) = CoroutineScope(Dispatchers.IO).launch {
         try {
-            when (day){
-                "Mardi" -> {
-                    val menuList: ArrayList<String> = ArrayList()
-                    val doc = Jsoup.connect("https://standarddelunivers.wordpress.com/2022/06/28/menu-de-la-semaine/").get()
-                    val tables = doc.select("table")
-                    val dayToShow = tables[0].select("th")[0].text()
-                    val rows = tables[0].select("tr")
-                    for (row in rows) {
-                        val cells = row.select("td")
-                        if (cells.size == 2) {
-                            val plat = cells[0].text()
-                            menuList.add(plat)
-                        }
-                    }
-                    withContext(Dispatchers.Main) {
-                        // Set the adapter for the ListView
-                        runOnUiThread {
-                            dayView.text = StringBuilder(dayToShow)
-                            menuCurrentState.text = getString(R.string.noMenuAvailable)
-                            menuListView.visibility = View.VISIBLE
-                            menuCurrentState.visibility = View.GONE
-                            menuListView.adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
-                        }
-                    }
-                }
-                "Mercredi" -> {
-                    val menuList: ArrayList<String> = ArrayList()
-                    val doc = Jsoup.connect("https://standarddelunivers.wordpress.com/2022/06/28/menu-de-la-semaine/").get()
-
-                    val tables = doc.select("table")
-                    val dayToShow = tables[0].select("th")[1].text()
-                    val rows = tables[0].select("tr")
-                    for (row in rows) {
-                        val cells = row.select("td")
-                        if (cells.size == 2) {
-                            val plat = cells[1].text()
-                            menuList.add(plat)
-                        }
-                    }
-                    withContext(Dispatchers.Main) {
-                        // Set the adapter for the ListView
-                        runOnUiThread {
-                            dayView.text = StringBuilder(dayToShow)
-                            menuCurrentState.text = getString(R.string.noMenuAvailable)
-                            menuListView.visibility = View.VISIBLE
-                            menuCurrentState.visibility = View.GONE
-                            menuListView.adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
-                        }
-                    }
-                }
-                "Jeudi" -> {
-                    val menuList: ArrayList<String> = ArrayList()
-                    val doc = Jsoup.connect("https://standarddelunivers.wordpress.com/2022/06/28/menu-de-la-semaine/").get()
-
-                    val tables = doc.select("table")
-                    val dayToShow = tables[1].select("th")[0].text()
-                    val rows = tables[1].select("tr")
-                    for (row in rows) {
-                        val cells = row.select("td")
-                        if (cells.size == 2) {
-                            val plat = cells[0].text()
-                            menuList.add(plat)
-                        }
-                    }
-                    withContext(Dispatchers.Main) {
-                        // Set the adapter for the ListView
-                        runOnUiThread {
-                            dayView.text = StringBuilder(dayToShow)
-                            menuCurrentState.text = getString(R.string.noMenuAvailable)
-                            menuListView.visibility = View.VISIBLE
-                            menuCurrentState.visibility = View.GONE
-                            menuListView.adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
-                        }
-                    }
-                }
-                "Vendredi" -> {
-                    val menuList: ArrayList<String> = ArrayList()
-                    val doc = Jsoup.connect("https://standarddelunivers.wordpress.com/2022/06/28/menu-de-la-semaine/").get()
-
-                    val tables = doc.select("table")
-                    val dayToShow = tables[1].select("th")[1].text()
-                    val rows = tables[1].select("tr")
-                    for (row in rows) {
-                        val cells = row.select("td")
-                        if (cells.size == 2) {
-                            val plat = cells[1].text()
-                            menuList.add(plat)
-                        }
-                    }
-                    withContext(Dispatchers.Main) {
-                        // Set the adapter for the ListView
-                        runOnUiThread {
-                            dayView.text = StringBuilder(dayToShow)
-                            menuCurrentState.text = getString(R.string.noMenuAvailable)
-                            menuListView.visibility = View.VISIBLE
-                            menuCurrentState.visibility = View.GONE
-                            menuListView.adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
-                        }
-                    }
-                }
-                else -> {
-                    withContext(Dispatchers.Main) {
-                        runOnUiThread {
-                            dayView.text = StringBuilder(day)
-                            val temp = getString(R.string.noMenuAvailableOn)
-                            menuListView.visibility = View.GONE
-                            menuCurrentState.visibility = View.VISIBLE
-                            menuCurrentState.text = "$temp le $day"
-                        }
-                    }
+            val menuList: ArrayList<String> = ArrayList()
+            val doc = Jsoup.connect("https://standarddelunivers.wordpress.com/2022/06/28/menu-de-la-semaine/").get()
+            val tables = doc.select("table")
+            val dayToShow = tables[table].select("th")[cell].text()
+            val rows = tables[table].select("tr")
+            for (row in rows) {
+                val cells = row.select("td")
+                if (cells.size == 2) {
+                    val plat = cells[cell].text()
+                    menuList.add(plat)
                 }
             }
-
-
-
+            withContext(Dispatchers.Main) {
+                // Set the adapter for the ListView
+                runOnUiThread {
+                    dayView.text = StringBuilder(dayToShow)
+                    menuCurrentState.text = getString(R.string.noMenuAvailable)
+                    menuListView.visibility = View.VISIBLE
+                    menuCurrentState.visibility = View.GONE
+                    menuListView.adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
+                }
+            }
         } catch (e: IOException) {
-            runOnUiThread {
-                menuCurrentState.text = getString(R.string.noMenuAvailable)
+            withContext(Dispatchers.Main) {
+                runOnUiThread {
+                    menuCurrentState.text = getString(R.string.noMenuAvailable)
+                    menuListView.visibility = View.GONE
+                    menuCurrentState.visibility = View.VISIBLE
+                }
             }
+        }
+    }
 
+    @SuppressLint("SetTextI18n")
+    private fun executeForXDay(day: String) = CoroutineScope(Dispatchers.IO).launch {
+        when (day){
+            "Mardi" -> {
+                getMenu(0,0)
+            }
+            "Mercredi" -> {
+                getMenu(0,1)
+            }
+            "Jeudi" -> {
+                getMenu(1,0)
+            }
+            "Vendredi" -> {
+                getMenu(1,0)
+            }
+            else -> {
+                withContext(Dispatchers.Main) {
+                    runOnUiThread {
+                        dayView.text = StringBuilder(day)
+                        menuCurrentState.text = getString(R.string.noMenuAvailable)+" "+day
+                        menuListView.visibility = View.GONE
+                        menuCurrentState.visibility = View.VISIBLE
+                    }
+                }
+            }
         }
     }
 }
